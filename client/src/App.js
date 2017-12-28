d7c0717c4b07b7ad7caf640010ab845accaf7006
rachd <rachel.m.dorn@gmail.com>
2017-12-28 18:32:58 -0500

    add frontend

---
+++ client/src/App.js
@@ -0,0 +1,55 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+import get from './services/api';
+import {dummyData} from './services/dummy';
+import {HOUSES} from './services/houses';
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      houses: null
+    }
+  }
+  componentDidMount() {
+    this.getData();
+  }
+
+  getData = () => {
+    fetch('/api/leaderboard').then(res => res.json()).then(data => this.processData(data.leaderboard));
+  }
+
+  processData(data) {
+    let houseCounts = [0, 0, 0, 0];
+    data.map((person) => {
+      const house = HOUSES[person.username];
+      if (house != undefined) {
+        houseCounts[house]+= parseInt(person.count);
+      }
+    });
+    this.setState({houses: houseCounts});
+  }
+
+  render() {
+    let bodyData = <div className="body"><p>loading</p></div>;
+    if (this.state.houses != null) {
+      bodyData = <div className="body">
+        <p>Ravenclaw: {this.state.houses[0]}</p>
+        <p>Slytherin: {this.state.houses[1]}</p>
+        <p>Gryffindor: {this.state.houses[2]}</p>
+        <p>Hufflepuff: {this.state.houses[3]}</p>
+      </div>
+    }
+    return (
+      <div className="App">
+        <header className="App-header">
+          <h1 className="App-title">Authentic House Cup</h1>
+        </header>
+          {bodyData}
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ client/src/services/houses.js
@@ -0,0 +1,13 @@
+// Ravenclaw 0
+// Slytherin 1
+// Gryffindor 2
+// Hufflepuff 3
+export const HOUSES = {
+    "stefano": 0,
+    "rachel-dorn": 0,
+    "katdroke": 2,
+    "hollymyles": 0,
+    "kateelane": 3,
+    "molly": 1,
+    "molly.underwood": 1
+}
